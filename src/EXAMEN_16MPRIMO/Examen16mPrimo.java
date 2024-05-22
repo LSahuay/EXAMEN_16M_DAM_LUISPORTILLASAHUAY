e4c4004bbd016112d602e62126b885efd90158c8
luisj <luisj@LuisPc>
2024-05-22 12:00:34 +0200

    Actualizacion repositorio Master

---
+++ src/EXAMEN_16MPRIMO/Examen16mPrimo.java
@@ -0,0 +1,12 @@
+package EXAMEN_16MPRIMO;
+
+public class Examen16mPrimo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Buenos días CEINMARK, soy Luis Portilla");
+		System.out.println("Introduce un núñmero primo");
+
+	}
+
+}
